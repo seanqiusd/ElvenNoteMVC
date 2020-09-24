b5b5d121bbd4b797534a3b6475a977f26242010e
seansql <shawnq2116@gmal.com>
2020-09-24 17:30:14 -0400

    NoteController DeleteNote Functionality Built

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -100,6 +100,22 @@ namespace ElevenNote.Services
             }
         }
 
+        // Delete -- Note
+        public bool DeleteNote(int noteId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == noteId && e.OwnerId == _userId);
+
+                ctx.Notes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
 
 
     }
